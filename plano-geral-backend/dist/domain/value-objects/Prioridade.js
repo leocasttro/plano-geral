c95935c8bb9dd079d592ac1f9ff5f3bc19b842b8
leocasttro <leocasttro01@icloud.com>
2026-07-22 19:04:15 -0300

    chore(git): ignorar arquivos gerados de build e cache

--- plano-geral-backend/dist/domain/value-objects/Prioridade.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Prioridade = void 0;
-exports.Prioridade = {
-    BAIXA: 'BAIXA',
-    MEDIA: 'MEDIA',
-    ALTA: 'ALTA',
-    CRITICA: 'CRITICA',
-};
